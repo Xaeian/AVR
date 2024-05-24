22f9683e01eb3193261cfd7c38c0d483f3c7cd93
Xaeian <git@sqrt.pl>
2024-05-24 17:07:16 +0200

    Template main.c

--- main.c
+++ main.c
@@ -1,32 +1,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdbool.h>
-
-#define LED_ON (PORTD |= (1 << 2))
-#define LED_OFF (PORTD &= ~(1 << 2))
 
 int main(void)
 {
   // init();
-  DDRD |= (1 << 2); // PD2 Output
-  DDRB &= ~(1 << 1); // PB1 Input
-  PORTB |= (1 << 1); // PB1 Pull up
+  DDRD |= (1 << 3);
 
-  int i;
-  bool blink = false;
-
-  while(1)
+  while (1)
   {
-    if(!(PINB & (1 << 1))) {
-      blink = !blink;
-    }
-    //------------------------
-    if(blink) {
-      PORTD ^= (1 << 2);
-    }
-    else {
-      PORTD &= ~(1 << 2);
-    }
-    _delay_ms(100);
+    // loop();
+    _delay_ms(200);
+    PORTD |= (1 << 3);
+    _delay_ms(200);
+    PORTD &= ~(1 << 3);
   }
 }
